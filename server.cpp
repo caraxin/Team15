5c71f730492b76491eb47bc5a566391275bf146e
Nathaniel Chu <nathanielchu4@gmail.com>
2017-01-24 01:44:55 -0800

    Added configuration files

--- server.cpp
+++ server.cpp
@@ -1,5 +1,18 @@
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <boost/asio.hpp>
 
 #include "server.h"
+
+using boost::asio::ip::tcp;
+
+std::string make_daytime_string()
+{
+  using namespace std; // For time_t, time and ctime;
+  time_t now = time(0);
+  return ctime(&now);
+}
 
 namespace Team15 {
 namespace server {
@@ -8,6 +21,29 @@ namespace server {
   }
   
   void server::run() {
+	  try
+	  {
+	    boost::asio::io_service io_service;
+
+	    tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 13));
+
+	    for (;;)
+	    {
+	      tcp::socket socket(io_service);
+	      acceptor.accept(socket);
+
+	      std::string message = make_daytime_string();
+
+	      boost::system::error_code ignored_error;
+	      boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
+	    }
+	  }
+	  catch (std::exception& e)
+	  {
+	    std::cerr << e.what() << std::endl;
+	  }
+
+	  return;
   }
 
 }

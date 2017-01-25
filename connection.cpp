c4ee803a6760d544031d02a54b723db73cce0c85
Nathaniel Chu <nathanielchu4@gmail.com>
2017-01-25 03:15:47 -0800

    returns HttpResponse, but content type is not implemented yet

--- connection.cpp
+++ connection.cpp
@@ -1,5 +1,5 @@
-
-
+#include "HttpRequest.h"
+#include "HttpResponse.h"
 #include "connection.h"
 
 namespace Team15 {
@@ -16,18 +16,27 @@ namespace server {
   void connection::start_reading() {
     //auto self(this->shared_from_this());
     socket_.async_read_some(boost::asio::buffer(buffer_),
-			    [this]
-			    (boost::system::error_code ec, std::size_t bytes)
-			    {
-			      if (!ec) {
-				std::string str(buffer_.begin(),buffer_.end());
-				printf("%s",str.c_str());
-				start_writing();
-			      }
-			      
-			    });
+      [this]
+      (boost::system::error_code ec, std::size_t bytes)
+      {
+        if (!ec) {
+          start_writing();
+        }
+        
+      });
   }
   void connection::start_writing() {
+    std::string str(buffer_.begin(),buffer_.end());
+    char * wire = new char[str.length()+1];
+    strcpy(wire, str.c_str());
+
+    HttpResponse* response = new HttpResponse("", "", wire, str.length()+1);
+    response->setStatusCode(OK);
+    response->setContentType("text/plain");
+
+    wire = response->toText();
+    std::cout << wire << std::endl;
+
     boost::system::error_code ignored_ec;
     socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_both, ignored_ec);
   }
